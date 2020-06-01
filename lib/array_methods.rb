185fc0a9d268c5ab50c26759244821eda9e90450
Nathan Dorazil <nathand1915@gmail.com>
2020-06-01 00:50:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,6 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  element = "wow"
-  array.include?(element)
+  array.include?("wow")
 end
 
 def using_sort(array)
